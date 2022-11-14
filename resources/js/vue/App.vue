46aa1d9227c097e25c94dbf6692d15429f6faa26
juliannavia <juliannavia157@hotmail.es>
2022-11-14 00:33:57 -0500

    Control de barra de navegacion

--- resources/js/vue/App.vue
+++ resources/js/vue/App.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <LandingPage></LandingPage>
-    </div>
+  <div>
+    <LandingPage></LandingPage>
+  </div>
 </template>
 
 <script>
@@ -9,8 +9,29 @@
 import LandingPage from './components/landing-page/Landing-page.vue'
 
 export default {
+    data(){
+        return {
+            isLoggedIn: false,
+            token: "",
+            user: "",
+        }
+    },
     components: {
     LandingPage
+    },
+methods:{
+    setCookieAuth(data){
+      console.log("entro");
+      this.$cookies.set("auth", data);
+      this.isLoggedIn = data.isLoggedIn;
+      this.token = data.token;
+      this.user = data.user;
+      console.log(data);
+      console.log(this.isLoggedIn);
+      console.log(this.token);
+      console.log(this.user);
+      console.log(this.$cookies.get("auth"));
+    },
 },
 }
 </script>

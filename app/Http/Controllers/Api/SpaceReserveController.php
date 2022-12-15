cbf77e7a7e9bc074d7fcf8a4fb2f9fbacce80b6d
juliannavia <juliannavia157@hotmail.es>
2022-12-14 22:07:16 -0500

    Reserves spaces backend

--- app/Http/Controllers/Api/SpaceReserveController.php
+++ app/Http/Controllers/Api/SpaceReserveController.php
@@ -12,4 +12,34 @@ class SpaceReserveController extends Controller
         $reserves = SpaceReserves::where("space_id",$space_id)->get();
         return response()->json($reserves);
     }
+
+    public function deleteSpaceReserve(Request $request){
+        $id = $request['reserve_id'];
+        $reserve = SpaceReserves::where("id",$id)->firstOrFail();
+        $reserve->delete();
+        return response()->json("ok");
+    }
+
+    public function setSpacesReserves(Request $request){
+        //SpaceReserves::create($request->toArray())
+        $date = $request['date'];
+        $reserve = $request['reserve'];
+        $num_trim = strpos($date, "T");
+        $date_trim = substr($date, 0 , $num_trim); 
+        $time_trim = substr($date, $num_trim + 1, 8); 
+
+        $reserves = SpaceReserves::where("space_id",$request['space_id'])->get();
+
+        for ($i = 0; $i <= (count($reserves) - 1); $i++) {
+            $date_reserve = substr($reserves[$i]['date'], 0 , $num_trim);
+            $time_reserve = $reserves[$i]['reserve'];
+            if(strcasecmp($date_reserve,$date_trim) == 0){
+                if(strcasecmp($time_reserve,$reserve) == 0){
+                    echo("Hello");
+                    return response()->json("ERROR : Ya hay una reserva con esta fecha y hora");
+                } 
+            } 
+        }
+        return response()->json(SpaceReserves::create($request->toArray()));
+    }
 }
